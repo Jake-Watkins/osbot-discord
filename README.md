61b4899eddeb365104275156190cfc6659adc08d
jake <watkinj7@wwu.edu>
2018-12-16 13:38:48 -0800

    checks if link is valid

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # osbot-discord
+
+intended features:
+  1. respond to $add itemname to add to users total loot
+  2. this will lookup the items value using the osrs ge api and store this value along with a the screenshot to a googlesheets doc
+  3. respond to $lookup user
+  4. this will sum the total loot at the time of drop for this user
--- bot.py
+++ bot.py
@@ -1,9 +1,17 @@
-# Work with Python 3.6
+#Version 0.1
 import discord
+from urllib.request import urlopen
+import sys
 
 TOKEN = 'NTIzOTUxNDczMjEwNTU2NDE2.DvhEDA.cBUM5PjFaVPgDWLd-PNVXP3qsz8'
 
 client = discord.Client()
+
+def processMessage(string):
+    link = string.split(" ")[-1]
+    item = string.rsplit(' ', 1)[0].split(' ', 1)[1]
+    return item, link
+
 
 @client.event
 async def on_message(message):
@@ -11,9 +19,35 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('!hello'):
-        msg = 'Hello {0.author.mention}'.format(message)
+    if message.content.startswith('$help'):
+        msg = 'need to impliment'.format(message)
         await client.send_message(message.channel, msg)
+
+
+    if message.content.startswith('$add'):
+        item, link = processMessage(message.content)
+        try :
+            ##check if valid link
+            urlopen(link)
+
+            ##check if valid item & price
+            
+
+
+            msg = 'Adding '+ item +' to user {0.author.mention}'.format(message)
+            await client.send_message(message.channel, msg)
+
+        except:
+            print("Unexpected error:", sys.exc_info()[0])
+            msg = 'last item was not a valid link, please try again with format $add item name link: '.format(message)
+            await client.send_message(message.channel, msg)
+
+
+    if message.content.startswith('$lookup'):
+        msg = 'need to impliment'.format(message)
+        await client.send_message(message.channel, msg)
+
+
 
 @client.event
 async def on_ready():
