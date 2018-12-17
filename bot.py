75b6a83fe7a1606a492d345471dcdafbb85e2144
jake <watkinj7@wwu.edu>
2018-12-16 17:06:14 -0800

    wow it works!

--- bot.py
+++ bot.py
@@ -3,6 +3,11 @@ import discord
 from urllib.request import urlopen
 import sys
 import json
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import pprint
+import datetime
+import traceback
 
 TOKEN = 'NTIzOTUxNDczMjEwNTU2NDE2.DvhEDA.cBUM5PjFaVPgDWLd-PNVXP3qsz8'
 
@@ -10,9 +15,13 @@ TOKEN = 'NTIzOTUxNDczMjEwNTU2NDE2.DvhEDA.cBUM5PjFaVPgDWLd-PNVXP3qsz8'
 ##and just update once a day
 ItemIdDBUrl = "https://rsbuddy.com/exchange/names.json"
 PriceUrl = "http://services.runescape.com/m=itemdb_oldschool/api/catalogue/detail.json?item="
-
-
 client = discord.Client()
+scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+creds = ServiceAccountCredentials.from_json_keyfile_name('OsBot.json', scope)
+gspreadclient = gspread.authorize(creds)
+sheet = gspreadclient.open('CC Drops').sheet1
+pp = pprint.PrettyPrinter()
+
 
 def processMessage(str):
     link = str.split(" ")[-1]
@@ -51,7 +60,7 @@ def findValue(id):
     price = 0
     with urlopen(PriceUrl + id) as url:
         data = json.loads(url.read().decode())
-    price = data['item']['current']['price']
+    price = str(data['item']['current']['price'])
     value = convertToNumber(price.lower().strip())
     return price, value
 
@@ -82,11 +91,17 @@ async def on_message(message):
             #find value of item
 
             shortprice,value = findValue(ItemId)
-
-            msg = 'Adding '+ item +' to user {0.author.mention} '.format(message) + ' value: ' + shortprice
+            print("price is valid")
+            msg = 'Adding '+ item +' to user {0.author.mention}'.format(message) + ' value: ' + shortprice
+            print("message is valid")
+            now = datetime.datetime.now()
+            date = [str(now.month),'/' ,str(now.day),'/', str(now.year)]
+            row = ['{0.author.mention} '.format(message),item, value, link, "".join(date)]
+            sheet.insert_row(row, 2)
             await client.send_message(message.channel, msg)
 
         except:
+            traceback.print_exc()
             print("Unexpected error:", sys.exc_info()[0])
             msg = 'last item was not a valid link, please try again with format $add item name link: '.format(message)
             await client.send_message(message.channel, msg)
